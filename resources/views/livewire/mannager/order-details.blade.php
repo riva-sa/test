34683dbaef0835e3607cb3468e25b214fe2e6031
mohamed elshamy <drawinglifestyle6@gmail.com>
2025-05-22 08:17:21 +0300

    btn hover color

--- resources/views/livewire/mannager/order-details.blade.php
+++ resources/views/livewire/mannager/order-details.blade.php
@@ -366,7 +366,7 @@
                             @if (!$isEditingMessage && $order->message)
                                 <div class="flex space-x-1 space-x-reverse">
                                     <button wire:click="startEditMessage"
-                                            class="text-primary-600 hover:text-primary-800 p-1 rounded hover:bg-primary-100"
+                                            class="text-primary-600 hover:text-white p-1 rounded hover:bg-primary-100"
                                             title="تعديل الملاحظة">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
