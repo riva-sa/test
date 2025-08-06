5a26b80dd0f6f3dea02d1ecae19825e08d0abe48
mohamed elshamy <64151860+mohamedElshamy00000@users.noreply.github.com>
2025-08-06 19:23:54 +0300

    user role in order permitions

--- resources/views/livewire/mannager/order-permissions.blade.php
+++ resources/views/livewire/mannager/order-permissions.blade.php
@@ -99,7 +99,7 @@
                 </div>
 
                 <!-- Permission Type -->
-                <div class="hidden">
+                <div>
                     <label class="block text-sm font-medium text-gray-600 mb-1 flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 ml-1 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
