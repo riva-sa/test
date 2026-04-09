922e21859ecf63c93cf4446c33915bec068e63b6
mohamed elshamy <64151860+mohamedElshamy00000@users.noreply.github.com>
2026-04-09 16:16:01 +0200

    style: update status color for pending orders to green

--- resources/views/livewire/mannager/manage-orders.blade.php
+++ resources/views/livewire/mannager/manage-orders.blade.php
@@ -294,7 +294,7 @@
                                 <div class="flex items-center gap-2">
                                     @php
                                         $statusColors = [
-                                            0 => 'bg-blue-100 text-blue-800',
+                                            0 => 'bg-green-300 text-green-800',
                                             1 => 'bg-yellow-100 text-yellow-800',
                                             2 => 'bg-green-100 text-green-800',
                                             3 => 'bg-gray-100 text-gray-800',
